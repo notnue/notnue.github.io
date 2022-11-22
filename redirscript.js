78f527600220bad59f428bd3790528427861444b
Neelesh Patel <84118988+notnue@users.noreply.github.com>
2022-11-22 22:29:36 +0530

    Update redirscript.js

--- redirscript.js
+++ redirscript.js
@@ -6,6 +6,6 @@ setInterval(()=>{
     counter.innerText = count;
     count++
     
-    if(count > 3) location.replace('https://bing.com')
+    if(count > 3) location.replace('https://www.google.com/')
     
 },1000)
